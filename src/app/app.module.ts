625d8a8799b96dc27794041d28f0bccb9233a0b8
saccanimattia <mattia.saccani@studenti.iispascal.it>
2023-06-05 10:29:03 +0200

    aggiunto update elimina e aggiungi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatInputModule } from '@angular/material/input';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TabellaClassiQuarteComponent } from './tabella-classi-quarte/tabella-classi-quarte.component';
-import { ClasseComponent } from './classe/classe.component';
+import { TabellaClassiQuarteComponent } from './componenti/tabella-classi-quarte/tabella-classi-quarte.component';
+import { ClasseComponent } from './componenti/classe/classe.component';
+import { InputNuovoStudenteComponent } from './componenti/input-nuovo-studente/input-nuovo-studente.component';
+import { FormsModule } from '@angular/forms';
+import { InputEliminaStudenteComponent } from './componenti/input-elimina-studente/input-elimina-studente.component';
+import { InputCreaClasseComponent } from './componenti/input-crea-classe/input-crea-classe.component';
+import { InputUpdateStudenteComponent } from './componenti/input-update-studente/input-update-studente.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TabellaClassiQuarteComponent,
     ClasseComponent,
+    InputNuovoStudenteComponent,
+    InputEliminaStudenteComponent,
+    InputCreaClasseComponent,
+    InputUpdateStudenteComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
