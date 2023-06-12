e03617013b5306367245cfe66c0c2a4a70167166
saccanimattia <mattia.saccani@studenti.iispascal.it>
2023-06-12 15:27:47 +0200

    fi

--- src/app/componenti/tabelle/tabella-classi-quarte/tabella-classi-quarte.component.ts
+++ src/app/componenti/tabelle/tabella-classi-quarte/tabella-classi-quarte.component.ts
@@ -1,94 +1,89 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { PrendiDatiService } from '../../../servizi/prendi-dati.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { PocketBaseService } from '../../../servizi/pocket-base-service.service'
+import { PocketBaseService } from '../../../servizi/pocket-base-service.service';
 import { Subscription } from 'rxjs';
 import { ArrayServiceService } from '../../../servizi/array-service.service';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 
-
-declare var window : any
+declare var window: any;
 
 @Component({
   selector: 'app-tabella-classi-quarte',
   templateUrl: './tabella-classi-quarte.component.html',
   styleUrls: ['./tabella-classi-quarte.component.scss']
 })
-export class TabellaClassiQuarteComponent implements OnInit{
+export class TabellaClassiQuarteComponent implements OnInit {
   classiQuarte: any[] = [];
-  classeSelezionata: any
-  selectedClasses: any[] = []
-  isClasse: any
-  formModal:any
+  classeSelezionata: any;
+  selectedClasses: any[] = [];
+  isClasse: any;
+  formModal: any;
   private studentsSubscription!: Subscription;
-  x:any
+  x: any;
   searchQuery: string = '';
   filteredArray: any[] = [];
   classi: any[] = [];
   @ViewChild(MatPaginator) paginator!: MatPaginator;
-  ord = "bi bi-caret-down"
-  constructor(private prendi: PrendiDatiService, private router : Router, private pocketBaseService: PocketBaseService, private arr:ArrayServiceService) {
+  ord = "bi bi-caret-down";
+  page = 0;
+  Npage = 5
 
-  }
+  constructor(
+    private prendi: PrendiDatiService,
+    private router: Router,
+    private pocketBaseService: PocketBaseService,
+    private arr: ArrayServiceService
+  ) {}
 
   ngOnInit(): void {
     this.prendiClassi();
 
-    this.studentsSubscription = this.pocketBaseService.subscribeToStudentsChanges()
-      .subscribe({
-        next: (student) => {
-          console.log("Dentro la sottoscrizione", student);
-          this.x = student;
-        },
-        error: (error) => {
-          console.log("Errore durante la sottoscrizione", error);
-        },
-        complete: () => {
-          console.log("Sottoscrizione completata");
-          console.log("this.x:", this.x);
-        }
-      });
-
+    this.studentsSubscription = this.pocketBaseService.subscribeToStudentsChanges().subscribe({
+      next: (student) => {
+        console.log("Dentro la sottoscrizione", student);
+        this.x = student;
+      },
+      error: (error) => {
+        console.log("Errore durante la sottoscrizione", error);
+      },
+      complete: () => {
+        console.log("Sottoscrizione completata");
+        console.log("this.x:", this.x);
+      }
+    });
   }
 
   async prendiClassi(): Promise<void> {
     try {
-
       let quarte: any = await this.prendi.prendiClassi();
-      this.classiQuarte = quarte.items as any[]
-      this.filteredArray = this.classiQuarte
+      this.classiQuarte = quarte.items as any[];
+      this.filteredArray = this.classiQuarte;
       this.classi = this.filteredArray.slice(0, 5);
-      this.arr.inizializzaClassi(this.classiQuarte)
+      this.arr.inizializzaClassi(this.classiQuarte);
     } catch (err) {
       console.log("Si è verificato un errore:", err);
     }
   }
 
   visualizza(classe: any, index: number) {
-    const classeId = index + 1;
+    console.log(this.page)
+    console.log(index)
+    const classeId = (this.Npage*(this.page)) + index + 1;
     this.router.navigate(['/classe', classeId]);
   }
 
+  ngOnDestroy(): void {
+    this.studentsSubscription.unsubscribe();
+  }
 
-
-
-    ngOnDestroy(): void {
-      this.studentsSubscription.unsubscribe();
-    }
-
-
-
-
-
-    performSearch(): void {
-      this.filteredArray = this.classiQuarte
-      this.filteredArray = this.classiQuarte.filter(item => {
-        return item.name.toLowerCase().includes(this.searchQuery.toLowerCase());
-      });
-      this.classi = this.filteredArray.slice(0,this.paginator.pageSize)
-    }
-
-
+  performSearch(): void {
+    this.filteredArray = this.classiQuarte.filter((item) => {
+      return item.name.toLowerCase().includes(this.searchQuery.toLowerCase());
+    });
+    this.paginator.pageIndex = 0; // Reimposta l'indice di pagina a 0 quando si esegue la ricerca
+    this.classi = this.filteredArray.slice(0, this.paginator.pageSize);
+  }
 
   toggleSelected(classe: any) {
     if (classe.selected) {
@@ -99,43 +94,27 @@ export class TabellaClassiQuarteComponent implements OnInit{
         this.selectedClasses.splice(index, 1);
       }
     }
-
   }
-   modalChiuso(){
-    this.classiQuarte = this.arr.prendiClassi()
-    this.filteredArray = this.classiQuarte
-    this.classi = this.filteredArray.slice(0,this.paginator.pageSize)
-    this.selectedClasses = []
-   }
 
-   onPageChange(event: PageEvent) {
-    this.paginator.pageIndex = event.pageIndex;
-    this.paginator.pageSize = event.pageSize;
+  modalChiuso() {
+    this.classiQuarte = this.arr.prendiClassi();
+    this.filteredArray = this.classiQuarte;
+    this.classi = this.filteredArray.slice(0, this.paginator.pageSize);
+    this.selectedClasses = [];
+  }
+
+  onPageChange(event: PageEvent) {
     const startIndex = event.pageIndex * event.pageSize;
     const endIndex = startIndex + event.pageSize;
     this.classi = this.filteredArray.slice(startIndex, endIndex);
-    // Puoi aggiornare i dati visualizzati in base alla pagina corrente qui
+    this.page = event.pageIndex;
+    this.Npage = event.pageSize;
   }
 
-  ordina(){
-    this.classiQuarte = this.prendi.ordinaClassi(this.classiQuarte)
-    this.filteredArray = this.classiQuarte
-    this.classi = this.filteredArray.slice(0,this.paginator.pageSize)
-    this.ord = "bi bi-caret-down-fill"
+  ordina() {
+    this.classiQuarte = this.prendi.ordinaClassi(this.classiQuarte);
+    this.filteredArray = this.classiQuarte;
+    this.classi = this.filteredArray.slice(0, this.paginator.pageSize);
+    this.ord = "bi bi-caret-down-fill";
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
